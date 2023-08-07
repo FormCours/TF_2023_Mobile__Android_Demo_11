8e8d11d195b8567bff71c84f7855f5747c4c5334
Aurélien <aurelien.strimelle@bstorm.be>
2023-08-07 10:09:23 +0200

    Initial Commit

---
+++ app/src/main/java/be/tftic/web2023/demo11_async_webrequest/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package be.tftic.web2023.demo11_async_webrequest.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
