3155ee1e65e8329a475134c35d09cecdaf6e6046
Aurélien <aurelien.strimelle@bstorm.be>
2023-08-07 14:14:24 +0200

    Demo Coroutine

--- app/src/main/java/be/tftic/web2023/demo11_async_webrequest/ui/demoasync/DemoAsyncFragment.kt
+++ app/src/main/java/be/tftic/web2023/demo11_async_webrequest/ui/demoasync/DemoAsyncFragment.kt
@@ -5,29 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import be.tftic.web2023.demo11_async_webrequest.R
+import be.tftic.web2023.demo11_async_webrequest.databinding.FragmentDemoAsyncBinding
+import be.tftic.web2023.demo11_async_webrequest.databinding.FragmentHomeBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.time.LocalDateTime
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DemoAsyncFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DemoAsyncFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding : FragmentDemoAsyncBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -35,26 +30,77 @@ class DemoAsyncFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_demo_async, container, false)
+        binding = FragmentDemoAsyncBinding.inflate(inflater, container, false)
+
+        // Listener on big pink bouton
+        binding.btnFragAsyncStart.setOnClickListener { startCounterAsync2() }
+        binding.btnFragAsyncLife.setOnClickListener {
+            Toast.makeText(requireContext(), "Ui active :o", Toast.LENGTH_SHORT).show()
+        }
+
+        // Return view
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DemoAsyncFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DemoAsyncFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun startCounterAsync() {
+
+        lifecycleScope.launch(Dispatchers.Default) {
+            val after = LocalDateTime.now().plusSeconds(30)
+            while (after > LocalDateTime.now()) {
+
             }
+        }
+
+        // ↓ Thread async (IO)
+        lifecycleScope.launch(Dispatchers.IO) {
+
+            var counter = 0
+
+            while(counter <= 10) {
+
+                // ↓ Thread Main (UI)
+                lifecycleScope.launch(Dispatchers.Main){
+                    binding.tvFragAsyncCounter.text = getString(R.string.incr_counter, counter)
+                }
+
+                delay(1_000)
+                counter++
+            }
+        }
+    }
+
+
+    private fun startCounterAsync2() {
+        lifecycleScope.launch {
+            runCounter()
+
+            binding.tvFragAsyncCounter.setText(getString(R.string.counter_end))
+        }
+    }
+
+    // Le mot clef "suspend" permet de créer un fonction async
+    private suspend fun runCounter() {
+        // ← Ici, on est dans le thread de la coroutine qui utilise la fonction
+
+        // ↓ On déplace l'execution du code suivant dans un autre thread (IO)
+        withContext(Dispatchers.IO) {
+            // ← Ici, on est dans le thread IO
+            var counter = 0
+
+            while(counter <= 10) {
+
+                // Mise à jours du visuel
+                updateCounter(counter)
+
+                counter++
+                delay(1_000)
+            }
+        }
+    }
+
+    private suspend fun updateCounter(counter : Int) {
+        withContext(Dispatchers.Main) {
+            binding.tvFragAsyncCounter.text = getString(R.string.incr_counter, counter)
+        }
     }
 }
